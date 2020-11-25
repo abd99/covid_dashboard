4b80e5f66cdba42d19b47b6382f411bf63252bad
Abdullah Deshmukh <abdullahzakir99@gmail.com>
2020-11-25 12:20:34 +0530

    Refactor app directory

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:covid_dashboard/main.dart';
+import 'package:covid_dashboard/src/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(App());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
